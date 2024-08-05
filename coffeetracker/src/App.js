d7b612c2f06ef2a4b1204ccb1da57452ae54d98f
Weaver <cordies1106@gmail.com>
2024-08-06 00:01:16 +0300

    routing prep

--- coffeetracker/src/App.js
+++ coffeetracker/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import DefaultPage from './pages/DefaultPage';
+import { Navigate, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <ChakraProvider>
-      <DefaultPage/>
-    </ChakraProvider>
+    <Routes>
+      <Route path='/' element={<DefaultPage/>}></Route>
+    </Routes>
   );
 }
 
